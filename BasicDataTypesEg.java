8fa53221348725d6cd8142db281d0bb4f7d8ee2b
amancognisaas <aman@cognisaas.com>
2022-04-03 17:28:15 +0530

    init java programming

---
+++ BasicDataTypesEg.java
@@ -0,0 +1,39 @@
+public class BasicDataTypesEg {
+  public static void main(String []args) {
+    System.out.println("Hiiie to data types");
+
+    // boolean data types
+    /* boolean canVote = true;
+    Boolean couldVote = false;
+    System.out.println(canVote);
+    System.out.println(couldVote); */
+
+    // byte data type is a 8 bit signed integer ranging from -128 to 127
+    // byte b1 = 127;
+    // System.out.println(b1);
+
+    // short data types is a 16 bit signed integer ranging from -32768 to 32767
+    // short sh1 = 1250;
+    // System.out.println(sh1);
+
+    // int data type is a 32 bit signed integer ranging from 2^31 to 2^31 -1
+    // int i1 = 12455;
+    // int i2 = -345432;
+    // System.out.println(i1+i2);
+
+    // long data type is 64 bit signed integer ranging from 2^63 to 2^63 -1
+    // long l1 = 342;
+    // System.out.println(l1);
+
+    // float
+    float f1 = 0.04f;
+    System.out.println(f1);
+
+    //double
+    double f2 = 0.05;
+    System.out.println(f2);
+
+
+    //char data type
+  }
+}
---
+++ Loops.java
@@ -0,0 +1,52 @@
+public class Loops {
+  public static void main(String []args) {
+    // for loop to print first n natural numbers
+    // for(int i=1; i<=10; i++) {
+    //   System.out.println(i);
+    // }
+
+    // for each loop in java
+    // String[] proLangNames= {"c","c++","java","python"};
+    // for(String langName: proLangNames) {
+    //   System.out.println(langName);
+    // }
+
+    // print first 10 even numbers usig while loop
+    // byte num=-2;
+    // byte i=1;
+    // while(i<=11) {
+    //   num+=2;
+    //   i+=1;
+    //   System.out.println(num);
+    // }
+
+    // print dirst ten even numbers using do while loops
+    // byte num=0;
+    // byte i=0;
+    // do {
+    //   System.out.println(num);
+    //   num+=2;
+    //   i++;
+    // }while(i<=10);
+
+    // for star pyramid pattern -1
+    // for(int i=1; i<=5;i++) {
+    //   for(int j=0;j<i;j++) {
+    //     System.out.print("* ");
+    //   }
+    //   System.out.println();
+    // }
+
+    // for loop star pattern pyramid -2
+    // int pyramidLevels = 7;
+    // for(int i=pyramidLevels; i>=0; i--) {
+    //   for(int j=0;j<i;j++) {
+    //     System.out.print("* ");
+    //   }
+    //   System.out.println();
+    // }
+
+    
+
+  }
+}
